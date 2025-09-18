39e6335c2454ab1e9769398050d87db982c2a73c
unknown <yoooooooniverse@gmail.com>
2025-09-18 10:02:42 +0800

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ export const MetaData = {
         apple: '/icons/apple-touch-icon.png',
         badge: 'badge-white.png',
     },
+    manifest: '/manifest.json',
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
